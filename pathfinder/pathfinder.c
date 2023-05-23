cb55ada4c3a404913974884f0b5916cb4742874b
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-05-23 14:32:56 +0300

    pwm write

--- pathfinder/pathfinder.c
+++ pathfinder/pathfinder.c
@@ -1,6 +1,130 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "hardware/uart.h"
+#include <stdio.h>
+#include <string.h>
 
+#define MOTOR_COUNT 8
+
+// Function declaration
+double map_motor_value(int value);
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d);
+
+void set_motor_values(const int *motorValues)
+{
+    for (int i = 0; i < MOTOR_COUNT; ++i)
+    {
+        int motorValue = motorValues[i];
+
+        // Map the input motor value (0-100) to the PWM duty cycle value (6.25-8.25)
+        double mappedValue = map_motor_value(motorValue);
+
+        // Set the PWM frequency and duty cycle using the provided function
+        uint slice_num = pwm_gpio_to_slice_num(i + 2);
+        uint chan = pwm_gpio_to_channel(i + 2);
+        pwm_set_freq_duty(slice_num, chan, 50, mappedValue);
+    }
+}
+
+double map_motor_value(int value)
+{
+    double mappedValue = 6.25 + (value / 100.0) * 2.0;
+    return mappedValue;
+}
+
+int main()
+{
+    // Initialize the Pico SDK
+    stdio_init_all();
+
+    // Configure GPIO pins 3 to 10 for PWM
+    for (int pin = 2; pin <= 9; ++pin)
+    {
+        gpio_set_function(pin, GPIO_FUNC_PWM);
+    }
+
+    for (int i = 0; i < MOTOR_COUNT; ++i)
+    {
+        uint slice_num = pwm_gpio_to_slice_num(i + 2);
+        pwm_set_enabled(slice_num, true);
+    }
+
+    // Serial port configuration
+    uart_inst_t *uart_instance = uart1; // Use UART1 for port 16 and 17
+    const int baud_rate = 9600;
+
+    // Initialize the UART
+    uart_init(uart_instance, baud_rate);
+    gpio_set_function(16, GPIO_FUNC_UART); // Set GPIO 16 as UART RX
+    gpio_set_function(17, GPIO_FUNC_UART); // Set GPIO 17 as UART TX
+
+    // Set up the UART data format
+    uart_set_format(uart_instance, 8, UART_PARITY_NONE, 1);
+
+    // Motor values received over serial will be stored in this array
+    int motorValues[MOTOR_COUNT] = {0};
+
+    // Custom framing variables
+    char startMarker = '{';
+    char endMarker = '}';
+    char receivedMsg[256];
+    bool isUARTDataReceived = false;
+    int receivedIndex = 0;
+
+    while (true)
+    {
+        while (uart_is_readable(uart_instance))
+        {
+            char c = uart_getc(uart_instance);
+
+            if (c == startMarker)
+            {
+                // Start of a new packet, reset the buffer
+                memset(receivedMsg, 0, sizeof(receivedMsg));
+                receivedIndex = 0;
+                isUARTDataReceived = true;
+            }
+            else if (c == endMarker && isUARTDataReceived)
+            {
+                // End of the packet, process the received motor values
+                int motorIndex = 0;
+                char *token = strtok(receivedMsg, ",");
+                while (token != NULL && motorIndex < MOTOR_COUNT)
+                {
+                    motorValues[motorIndex] = atoi(token);
+                    motorIndex++;
+                    token = strtok(NULL, ",");
+                }
+
+                // Update the motor values based on the received packet
+                set_motor_values(motorValues);
+
+                // Reset the flags
+                isUARTDataReceived = false;
+            }
+            else if (isUARTDataReceived)
+            {
+                // Append the received character to the buffer
+                receivedMsg[receivedIndex++] = c;
+            }
+        }
+
+        // No UART input received, set all motor values to 50
+        if (!isUARTDataReceived)
+        {
+            for (int i = 0; i < MOTOR_COUNT; ++i)
+            {
+                motorValues[i] = 50;
+            }
+
+            // Update the motor values
+            set_motor_values(motorValues);
+        }
+    }
+    return 0;
+}
+
+// Set the PWM frequency and duty cycle for a given slice and channel
 uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d)
 {
     uint32_t clock = 125000000;
@@ -12,68 +136,4 @@ uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d)
     pwm_set_wrap(slice_num, wrap);
     pwm_set_chan_level(slice_num, chan, wrap * d / 100);
     return wrap;
-}
-
-int main()
-{
-    sleep_ms(5000);
-
-    gpio_set_function(3, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(3);
-    uint chan = pwm_gpio_to_channel(3);
-
-    pwm_set_freq_duty(slice_num, chan, 50, 7.5);
-    pwm_set_enabled(slice_num, true);
-    sleep_ms(5000);
-
-    pwm_set_freq_duty(slice_num, chan, 50, 6.5);
-    sleep_ms(1000);
-
-    pwm_set_freq_duty(slice_num, chan, 50, 8);
-    sleep_ms(2000);
-
-    // 6.25 min
-    // 7.5  mid
-    // 8.25 max
-
-    while (true)
-    {
-        for (int i = 0; i < 25; i++)
-        {
-            pwm_set_freq_duty(slice_num, chan, 50, 7.5 - (0.05 * i));
-            sleep_ms(200);
-        }
-        sleep_ms(1000);
-
-        for (int i = 0; i < 25; i++)
-        {
-            pwm_set_freq_duty(slice_num, chan, 50, 6.5 + (0.05 * i));
-            sleep_ms(200);
-        }
-
-        pwm_set_freq_duty(slice_num, chan, 50, 7.5);
-        sleep_ms(1000);
-
-        for (int i = 0; i < 25; i++)
-        {
-            pwm_set_freq_duty(slice_num, chan, 50, 7.5 + (0.05 * i));
-            sleep_ms(200);
-        }
-        sleep_ms(1000);
-
-        for (int i = 0; i < 25; i++)
-        {
-            pwm_set_freq_duty(slice_num, chan, 50, 8.25 - (0.05 * i));
-            sleep_ms(200);
-        }
-
-        pwm_set_freq_duty(slice_num, chan, 50, 7.5);
-        sleep_ms(2000);
-
-        pwm_set_freq_duty(slice_num, chan, 50, 7.7);
-        sleep_ms(1000);
-
-        pwm_set_freq_duty(slice_num, chan, 50, 7.5);
-        sleep_ms(2000);
-    }
 }
