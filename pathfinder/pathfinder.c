648eb0876e9057697f87e1ad5b408fe87fb170b0
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-05-30 19:20:33 +0300

    uart deprecated & i2c config

--- pathfinder/pathfinder.c
+++ pathfinder/pathfinder.c
@@ -1,43 +1,43 @@
-#include "pico/stdlib.h"
-#include "hardware/pwm.h"
-#include "hardware/uart.h"
 #include <stdio.h>
-#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pwm.h"
 
+#define I2C_CLIENT_ADDRESS 0x10
 #define MOTOR_COUNT 8
-#define UART_BUFFER_SIZE 256
 
-// Function declaration
-double map_motor_value(int value);
-uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d);
-
-void set_motor_values(const int *motorValues)
+// Function to map a value from one range to another
+double map(double value, double in_min, double in_max, double out_min, double out_max)
 {
-    for (int i = 0; i < MOTOR_COUNT; ++i)
-    {
-        int motorValue = motorValues[i];
-
-        // Map the input motor value (0-100) to the PWM duty cycle value (6.25-8.25)
-        double mappedValue = map_motor_value(motorValue);
-
-        // Set the PWM frequency and duty cycle using the provided function
-        uint slice_num = pwm_gpio_to_slice_num(i + 2);
-        uint chan = pwm_gpio_to_channel(i + 2);
-        pwm_set_freq_duty(slice_num, chan, 50, mappedValue);
-    }
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-double map_motor_value(int value)
+// Function to set the PWM frequency and duty cycle
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d)
 {
-    double mappedValue = 6.25 + ((value / 100.0) * 2.5);
-    return mappedValue;
+    uint32_t clock = 125000000;
+    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
+    if (divider16 / 16 == 0)
+        divider16 = 16;
+    uint32_t wrap = clock * 16 / divider16 / f - 1;
+    pwm_set_clkdiv_int_frac(slice_num, divider16 / 16, divider16 & 0xF);
+    pwm_set_wrap(slice_num, wrap);
+    pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+    return wrap;
 }
 
 int main()
 {
-    // Initialize the Pico SDK
     stdio_init_all();
 
+    i2c_init(i2c0, 100000);                             // Initialize I2C at 100kHz
+    gpio_set_function(16, GPIO_FUNC_I2C);               // Set GP16 as SDA
+    gpio_set_function(17, GPIO_FUNC_I2C);               // Set GP17 as SCL
+    i2c_set_slave_mode(i2c0, true, I2C_CLIENT_ADDRESS); // Set Pico as I2C client/server
+
+    uint8_t data[8];
+
+    // Initialize PWM
     // Configure GPIO pins 3 to 10 for PWM
     for (int pin = 2; pin <= 9; ++pin)
     {
@@ -50,106 +50,41 @@ int main()
         pwm_set_enabled(slice_num, true);
     }
 
-    // Serial port configuration
-    uart_inst_t *uart_instance = uart0; // Use UART1 for port 16 and 17
-    const int baud_rate = 9600;
-
-    // Initialize the UART
-    uart_init(uart_instance, baud_rate);
-    gpio_set_function(16, GPIO_FUNC_UART); // Set GPIO 16 as UART RX
-    gpio_set_function(17, GPIO_FUNC_UART); // Set GPIO 17 as UART TX
-    uart_set_hw_flow(uart_instance, false, false);
-
-    // Set up the UART data format
-    uart_set_format(uart_instance, 8, 1, UART_PARITY_NONE);
-
-    // Motor values received over serial will be stored in this array
-    int motorValues[MOTOR_COUNT] = {0};
-
-    // Custom framing variables
-    char startMarker = '{';
-    char endMarker = '}';
-    char receivedMsg[UART_BUFFER_SIZE];
-    bool isUARTDataReceived = false;
-    int receivedIndex = 0;
-
-    // FIFO buffer variables
-    char fifoBuffer[UART_BUFFER_SIZE];
-    int fifoHead = 0;
-    int fifoTail = 0;
-
     while (true)
     {
-        // Read incoming UART data and fill the FIFO buffer
-        while (uart_is_readable(uart_instance))
+        // Receive data from Pi 4
+        i2c_read_blocking(i2c0, I2C_CLIENT_ADDRESS, data, 8, false);
+
+        printf("Received data package: ");
+        for (int i = 0; i < 8; i++)
         {
-            char c = uart_getc(uart_instance);
-            fifoBuffer[fifoHead] = c;
-            fifoHead = (fifoHead + 1) % UART_BUFFER_SIZE;
+            printf("%d ", data[i]);
+        }
+        printf("\n");
+
+        // Process the received data and write to each motor
+        for (int i = 2; i <= 9; i++)
+        {
+            // Assuming data[i-2] represents the desired duty cycle for motor connected to pin i
+
+            // Map the motor value from 0-100 to the PWM range 6.25-8.75
+            double mappedValue = map(data[i - 2], 0, 100, 6.25, 8.75);
+
+            // Set motor PWM frequency to 1000Hz with mapped duty cycle
+            uint slice_num = pwm_gpio_to_slice_num(i);
+            pwm_set_freq_duty(slice_num, i, 50, mappedValue);
+
+            printf("Motor %d: Frequency = %d Hz, Duty Cycle = %f%%\n", i, 50, mappedValue);
         }
 
-        // Process characters in the FIFO buffer
-        while (fifoHead != fifoTail)
-        {
-            char c = fifoBuffer[fifoTail];
-            fifoTail = (fifoTail + 1) % UART_BUFFER_SIZE;
-
-            if (c == startMarker)
-            {
-                // Start of a new packet, reset the buffer and index
-                memset(receivedMsg, 0, sizeof(receivedMsg));
-                receivedIndex = 0;
-                isUARTDataReceived = true;
-                printf("Received new packet\n");
-            }
-            else if (c == endMarker && isUARTDataReceived)
-            {
-                // End of the packet, process the received data
-                printf("Received data: %s\n", receivedMsg);
-
-                // Tokenize the received data to extract motor values
-                char *token;
-                token = strtok(receivedMsg, ",");
-                int motorIndex = 0;
-                while (token != NULL && motorIndex < MOTOR_COUNT)
-                {
-                    motorValues[motorIndex] = atoi(token);
-                    printf("Motor %d value: %d\n", motorIndex, motorValues[motorIndex]);
-                    token = strtok(NULL, ",");
-                    motorIndex++;
-                }
-
-                // Update the motor values based on the received packet
-                set_motor_values(motorValues);
-
-                // Reset the flags and received index
-                isUARTDataReceived = false;
-                receivedIndex = 0;
-            }
-            else if (isUARTDataReceived)
-            {
-                // Append the received character to the buffer
-                receivedMsg[receivedIndex++] = c;
-                receivedMsg[receivedIndex] = '\0'; // Null-terminate the string
-            }
-        }
-
-        // No UART input received, set all motor values to 50
-        if (!isUARTDataReceived)
-        {
-            for (int i = 0; i < MOTOR_COUNT; ++i)
-            {
-                motorValues[i] = 50;
-            }
-
-            // Update the motor values
-            set_motor_values(motorValues);
-            printf("No UART input received. Setting motor values to 50.\n");
-        }
+        // Send a response back to Pi 4
+        i2c_write_blocking(i2c0, I2C_CLIENT_ADDRESS, data, 8, false);
     }
 
     return 0;
 }
+
+/*
 
 // Set the PWM frequency and duty cycle for a given slice and channel
 uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d)
@@ -164,3 +99,17 @@ uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d)
     pwm_set_chan_level(slice_num, chan, wrap * d / 100);
     return wrap;
 }
+
+// Configure GPIO pins 3 to 10 for PWM
+for (int pin = 2; pin <= 9; ++pin)
+{
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+}
+
+for (int i = 0; i < MOTOR_COUNT; ++i)
+{
+    uint slice_num = pwm_gpio_to_slice_num(i + 2);
+    pwm_set_enabled(slice_num, true);
+}
+
+*/
