4245a0861142eb7f6e3ab08f6ebdff4981b701b3
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-05-31 05:07:05 +0300

    irq + pwm write

--- pathfinder/pathfinder.c
+++ pathfinder/pathfinder.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/irq.h"
+#include "hardware/pwm.h"
 
 // define I2C addresses to be used for this peripheral
 #define I2C0_PERIPHERAL_ADDR 0x10
@@ -13,6 +14,24 @@
 #define BUFFER_SIZE 8
 uint8_t bytesReceived;
 uint8_t motorData[8];
+
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, double d)
+{
+    uint32_t clock = 125000000;
+    uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
+    if (divider16 / 16 == 0)
+        divider16 = 16;
+    uint32_t wrap = clock * 16 / divider16 / f - 1;
+    pwm_set_clkdiv_int_frac(slice_num, divider16 / 16, divider16 & 0xF);
+    pwm_set_wrap(slice_num, wrap);
+    pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+    return wrap;
+}
+
+double map(double value, double in_min, double in_max, double out_min, double out_max)
+{
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 // Interrupt handler implements the RAM
 void i2c0_irq_handler()
@@ -37,6 +56,19 @@ void i2c0_irq_handler()
             }
             printf("\n");
 
+            for (int i = 0; i < BUFFER_SIZE; ++i)
+            {
+                int motorValue = motorData[i];
+
+                // Map the input motor value (0-100) to the PWM duty cycle value (6.25-8.25)
+                double mappedValue = map(motorValue, 0, 100, 6.25, 8.75);
+
+                // Set the PWM frequency and duty cycle using the provided function
+                uint slice_num = pwm_gpio_to_slice_num(i + 2);
+                uint chan = pwm_gpio_to_channel(i + 2);
+                pwm_set_freq_duty(slice_num, chan, 50, mappedValue);
+            }
+
             bytesReceived = 0; // Reset byte counter
         }
     }
@@ -57,6 +89,18 @@ void i2c0_irq_handler()
 int main()
 {
     stdio_init_all();
+
+    // Configure GPIO pins 3 to 10 for PWM
+    for (int pin = 2; pin <= 9; ++pin)
+    {
+        gpio_set_function(pin, GPIO_FUNC_PWM);
+    }
+
+    for (int i = 0; i < BUFFER_SIZE; ++i)
+    {
+        uint slice_num = pwm_gpio_to_slice_num(i + 2);
+        pwm_set_enabled(slice_num, true);
+    }
 
     // Setup I2C0 as slave (peripheral)
     i2c_init(i2c0, 100 * 1000);
@@ -112,32 +156,5 @@ for (int i = 0; i < MOTOR_COUNT; ++i)
     uint slice_num = pwm_gpio_to_slice_num(i + 2);
     pwm_set_enabled(slice_num, true);
 }
-
-*/
-
-/*
-
-// Check to see if we have received data from the I2C controller
-    if (status & I2C_IC_INTR_STAT_R_RX_FULL_BITS)
-    {
-
-        // Receive FIFO full interrupt
-        while (i2c0_hw->status & I2C_IC_STATUS_RFNE_BITS)
-        {
-            motorData[bytesReceived++] = i2c0_hw->data_cmd;
-            if (bytesReceived >= BUFFER_SIZE)
-            {
-                // Process motor data
-                printf("Motor Data: ");
-                for (int i = 0; i < BUFFER_SIZE; i++)
-                {
-                    printf("%d ", motorData[i]);
-                }
-                printf("\n");
-
-                bytesReceived = 0; // Reset byte counter
-            }
-        }
-    }
 
 */
